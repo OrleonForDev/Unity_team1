cde149eff0138dc8f97e7cd82b9aef7452936170
OrleonForDev <AkchurinIS@gmail.com>
2023-04-08 18:30:32 +0400

    Добавил скрипт привет
    Hello programmers

---
+++ Assets/Testing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.Log("Hello unity programmers");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
